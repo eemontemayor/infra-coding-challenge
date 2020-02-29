108d8dee4a2d442db3b8c14fc1e44fcf6b480221
Enrique Montemayor <eemontemayor@live.com>
2020-02-28 21:05:13 -0600

    SSL handshake failing... still..

--- api/src/server.js
+++ api/src/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
+const UserService = require('./user-service')
 const mongo = require('./clients/mongo');
 const { sleep, generateRandomNum } = require('./utils');
 
@@ -11,18 +11,59 @@ server.use(bodyParser.json());
 server.get('/health', (_, res) => {
     return res.sendStatus(204);
 });
+// server.post('/users', async (req, res) => {
+//     const user = await mongo.user.create(req.body);
+//     await somethingSlow(user._id);
+    
+//     return res.status(200).json(user);
+// });
+
+
+
 
 server.post('/users', async (req, res) => {
-    const user = await mongo.user.create(req.body);
-    await somethingSlow(user._id);
+    const {firstName, lastName, email, username,password } = req.body
     
-    return res.status(200).json(user);
+    const newUser = {firstName, lastName, email, username,password }
+
+
+    // Check for fields
+    for (const [key, value] of Object.entries(newUser)) {
+        if (!value) {
+        return res.status(400).json({
+            error: `Missing ${key} in request body`
+        });
+        }
+    }
+
+
+    try {
+        
+        const user = await mongo.user.create(newUser);
+    
+        // await UserService.createUser(newUser)
+   
+    
+         somethingSlow(user._id);
+        
+        return res
+                .status(200)
+                 .json(user);
+        
+    } catch (error) {
+        
+            res.status(401).json({ error: 'Unauthorized request' });
+    }
 });
+
+
+
+
 
 async function somethingSlow(userID) {
     await mongo.user.updateOne({ _id: userID }, { $set: { processed: true } });
     
-    const sleepSeconds = generateRandomNum(5, 15);
+    const sleepSeconds = generateRandomNum(1,5);
     return await sleep(sleepSeconds * 1000);
 }
 
---
+++ api/src/user-service.js
@@ -0,0 +1,31 @@
+const mongo = require('./clients/mongo');
+
+
+const UserService = {
+   createUser(mongo,newUser) {
+
+        for (const [key, value] of Object.entries(req.body))
+        if (value == null)
+          return res.status(400).json({
+            error: `Missing '${key}' in request body`
+          })
+        try {
+            
+            const user = await mongo.user.create(newUser);
+    
+            if (!user) {
+                
+            }
+    
+            res.user = user
+            next()
+        }
+        catch(error) {
+            next(error)
+          }
+        
+    },
+
+}
+
+module.exports = {UserService}
