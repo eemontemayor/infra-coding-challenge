cc33fbb2fc7e0185a2010440cd96a826c7080e3a
Enrique Montemayor <eemontemayor@live.com>
2020-03-03 11:44:00 -0600

    test

--- api/src/user/user-router.js
+++ api/src/user/user-router.js
@@ -1,40 +1,40 @@
-// const express = require('express')
-// const mongo = require('../clients/mongo');
-// const userRouter = express.Router()
-// const userService = require('./user-service')
-// const bodyParser = require('body-parser');
-// mongo.Promise = global.Promise
+const express = require('express')
+const mongo = require('../clients/mongo');
+const userRouter = express.Router()
+const userService = require('./user-service')
+const bodyParser = require('body-parser');
+mongo.Promise = global.Promise
 
 
-// serializeUser = user => ({
-//     firstName: user.firstName,
-//     lastName: user.lastName,
-//     email: user.email,
-//     username: user.username,
-//     password: user,password
-// })
+serializeUser = user => ({
+    firstName: user.firstName,
+    lastName: user.lastName,
+    email: user.email,
+    username: user.username,
+    password: user,password
+})
 
-// userRouter  
-//     .route('/')
-//     .post(bodyParser, async (req, res) => {
+userRouter  
+    .route('/')
+    .post(bodyParser, async (req, res) => {
 
-//         const {firstName, lastName, email, username,password } = req.body
+        const {firstName, lastName, email, username,password } = req.body
     
-//         const newUser = {firstName, lastName, email, username,password }
+        const newUser = {firstName, lastName, email, username,password }
 
             
-//             let user = userService.createUser(newUser)
+            let user = userService.createUser(newUser)
               
-//             userService.somethingSlow(user._id);
+            userService.somethingSlow(user._id);
         
         
-//                 user
-//                 .then(user => res.status(201).json(serializeUser(user)))
-//                 .catch(err => {
-//                   console.error(err);
-//                   res.status(500).json({ message: "Internal server error" });
-//                 });
+                user
+                .then(user => res.status(201).json(serializeUser(user)))
+                .catch(err => {
+                  console.error(err);
+                  res.status(500).json({ message: "Internal server error" });
+                });
 
  
-//     })
-// module.exports = userRouter
+    })
+module.exports = userRouter;
--- api/src/user/user-service.js
+++ api/src/user/user-service.js
@@ -1,28 +1,28 @@
-// const mongo = require('../clients/mongo');
-// const { sleep, generateRandomNum } = require('../utils');
+const mongo = require('../clients/mongo');
+const { sleep, generateRandomNum } = require('../utils');
 
-// const userService = {
+const userService = {
     
 
 
-//     createUser(newUser) { 
-//         mongo.user.create({
-//             firstName: newUser.firstName,
-//             lastName: newUser.lastName,
-//             email: newUser.email,
-//             username: newUser.username,
-//             password: newUser,password
-//         })
+    createUser(newUser) { 
+        mongo.user.create({
+            firstName: newUser.firstName,
+            lastName: newUser.lastName,
+            email: newUser.email,
+            username: newUser.username,
+            password: newUser,password
+        })
 
 
-//     },
+    },
     
-//     async somethingSlow(userID) {
-//         await mongo.user.updateOne({ _id: userID }, { $set: { processed: true } });
+    async somethingSlow(userID) {
+        await mongo.user.updateOne({ _id: userID }, { $set: { processed: true } });
         
-//         const sleepSeconds = generateRandomNum(5,15);
-//         return await sleep(sleepSeconds * 1000);
-//     },
+        const sleepSeconds = generateRandomNum(5,15);
+        return await sleep(sleepSeconds * 1000);
+    },
 
-// }
-// module.exports= userService
+}
+module.exports = userService;
